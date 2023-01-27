52ddfee73d021b8f6d4327a4ba44d76d099e6ead
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-26 22:37:51 -0500

    Componente image
    Se crea código correspondiente en componente image para hacer el render de la foto del personaje

--- frontend-api-rick-and-morty/src/app/components/image/image.component.ts
+++ frontend-api-rick-and-morty/src/app/components/image/image.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit {
+
+
+  img: string = ''
+
+
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+  };
+
+
+  @Input() alt: string = '';
+  @Output() loaded = new EventEmitter<string>()
+  imageDefault: string = './assets/images/default.jpg';
+
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+
+  imgLoad() {
+    this.loaded.emit(this.img)
+  }
+
+
+  // Mostrar una imagen por defecto si hay un error al cargar la original
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
 
 }
+
