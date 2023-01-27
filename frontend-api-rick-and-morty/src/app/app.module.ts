f38d86ef0c1d41ec4acfd158dca695f835e96c2c
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-26 20:43:31 -0500

    Creación de nuevos componentes
    Se crean 4 nuevos componentes y se mueve componente principal a carpeta main

--- frontend-api-rick-and-morty/src/app/app.module.ts
+++ frontend-api-rick-and-morty/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { CharacterComponent } from './components/character/character.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { ImageComponent } from './components/image/image.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharacterListComponent,
+    ImageComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
