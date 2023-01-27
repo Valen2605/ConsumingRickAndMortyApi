3a34c228d956c0aed7db6f78016ecd47363090bc
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-26 22:00:26 -0500

    Componente Character
    Se crea el código correspondiente en el componente character para el render del personaje de la Api.

--- frontend-api-rick-and-morty/src/app/components/character/character.component.ts
+++ frontend-api-rick-and-morty/src/app/components/character/character.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Character, CharacterDTO } from 'src/app/models/character.model';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss']
 })
-export class CharacterComponent {
-
+/**
+ * Clase del componente para el personaje
+ */
+export class CharacterComponent  {
+  /**
+   * Input para recibir información desde el componente padre character-list
+   */
+  @Input() character!: CharacterDTO;
+  /**
+   * Output representa salida de datos hacía el componente padre character-list
+   */
+  @Output() addedCharacter = new EventEmitter<CharacterDTO>();
+    /**
+   * Output representa salida de datos hacía el componente padre character-list
+   */
+  @Output() showCharacterDetail = new EventEmitter<number>();
+/**
+ * Constructor de la clase
+ */
+  constructor() { }
+/**
+ * Metodo para emitir un personaje en el carrito de favoritos
+ */
+  onAddToCart() {
+    this.addedCharacter.emit(this.character);
+  }
+/**
+ * Metodo para emitir el detalle del personaje
+ */
+  onShowDetail() {
+    console.log('Estoy en detalle');
+    this.showCharacterDetail.emit(this.character.id);
+  }
 }
+
