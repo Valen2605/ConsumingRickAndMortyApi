8ee05a18f01f1708ae987ab9fb581118ac6d6fa6
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-26 22:43:26 -0500

    Componente nav
    Se crea código correspondiente en componente nav para renderizar el navbar de la aplicación

--- frontend-api-rick-and-morty/src/app/components/nav/nav.component.ts
+++ frontend-api-rick-and-morty/src/app/components/nav/nav.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+
+  activeSideMenu: boolean = false;
+  counter: number = 0;
+
+
+  constructor(
+     private storeService: StoreService
+     ) { }
+  ngOnInit(): void {
+    this.storeService.myCart$.subscribe(character => {
+    this.counter = character.length;
+    });
+  }
+
+
+  // Móstrar/ocultar side menu (mobile)
+  toggleSideMenu() {
+    this.activeSideMenu = !this.activeSideMenu;
+  }
+
 
 }
+
